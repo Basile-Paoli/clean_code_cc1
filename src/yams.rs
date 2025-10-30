e8706d538e06051efc43b24225d2533d7dc633f1
Basile Paoli <basile.paoli@gmail.com>
2025-10-30 14:15:49 +0100

    add chance case

--- src/yams.rs
+++ src/yams.rs
@@ -1,5 +1,21 @@
-fn calculate_yams_score(dice: &[u8; 5]) -> u32 {
-    28
+type Dice = [u8; 5];
+
+pub fn calculate_yams_score(dice: &Dice) -> u32 {
+    if contains_three_of_a_kind(dice) {
+        28
+    } else {
+        let sum: u8 = dice.iter().sum();
+        sum as u32
+    }
+}
+
+fn contains_three_of_a_kind(dice: &Dice) -> bool {
+    for value in dice {
+        if dice.iter().filter(|x| *x == value).count() >= 3 {
+            return true;
+        }
+    }
+    false
 }
 
 #[cfg(test)]
@@ -10,5 +26,11 @@ mod test {
     fn test_three_of_a_kind() {
         let dice = [3, 3, 3, 2, 5];
         assert_eq!(calculate_yams_score(&dice), 28);
+    }
+
+    #[test]
+    fn test_chance() {
+        let dice = [1, 2, 3, 4, 5];
+        assert_eq!(calculate_yams_score(&dice), 15);
     }
 }
