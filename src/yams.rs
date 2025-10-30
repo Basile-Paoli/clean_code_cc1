c70cdb016206b1f035062b3a378331aecc6918f0
Basile Paoli <basile.paoli@gmail.com>
2025-10-30 14:27:53 +0100

    add function to calculate over multiple rounds

--- src/yams.rs
+++ src/yams.rs
@@ -2,7 +2,12 @@ use std::collections::HashMap;
 
 type Dice = [u8; 5];
 
-pub fn calculate_yams_score(dice: &Dice) -> u32 {
+
+fn calculate_yams_total_score(rounds: &[Dice]) -> u32 {
+    rounds.iter().map(|dice| calculate_yams_round_score(dice)).sum()
+}
+
+fn calculate_yams_round_score(dice: &Dice) -> u32 {
     if contains_four_of_a_kind(dice) {
         35
     } else if contains_full_house(dice) {
@@ -43,29 +48,40 @@ fn contains_three_of_a_kind(dice: &Dice) -> bool {
 
 #[cfg(test)]
 mod test {
-    use super::calculate_yams_score;
+    use super::calculate_yams_round_score;
 
     #[test]
     fn test_three_of_a_kind() {
         let dice = [3, 3, 3, 2, 5];
-        assert_eq!(calculate_yams_score(&dice), 28);
+        assert_eq!(calculate_yams_round_score(&dice), 28);
     }
 
     #[test]
     fn test_four_of_a_kind() {
         let dice = [4, 4, 4, 4, 1];
-        assert_eq!(calculate_yams_score(&dice), 35);
+        assert_eq!(calculate_yams_round_score(&dice), 35);
     }
 
     #[test]
     fn test_full_house() {
         let dice = [2, 2, 3, 3, 3];
-        assert_eq!(calculate_yams_score(&dice), 30);
+        assert_eq!(calculate_yams_round_score(&dice), 30);
     }
 
     #[test]
     fn test_chance() {
         let dice = [1, 2, 3, 4, 5];
-        assert_eq!(calculate_yams_score(&dice), 15);
+        assert_eq!(calculate_yams_round_score(&dice), 15);
+    }
+
+    #[test]
+    fn test_yams_total_score() {
+        let rounds = vec![
+            [3, 3, 3, 2, 5], // Three of a kind: 28
+            [4, 4, 4, 4, 1], // Four of a kind: 35
+            [2, 2, 3, 3, 3], // Full house: 30
+            [1, 2, 3, 4, 5], // Chance: 15
+        ];
+        assert_eq!(super::calculate_yams_total_score(&rounds), 108);
     }
 }
