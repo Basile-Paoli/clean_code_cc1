bfe4e2b9e097669dd6137ef372e64694cc823705
Basile Paoli <basile.paoli@gmail.com>
2025-10-30 14:23:02 +0100

    add full_house case

--- src/yams.rs
+++ src/yams.rs
@@ -1,8 +1,12 @@
+use std::collections::HashMap;
+
 type Dice = [u8; 5];
 
 pub fn calculate_yams_score(dice: &Dice) -> u32 {
     if contains_four_of_a_kind(dice) {
         35
+    } else if contains_full_house(dice) {
+        30
     } else if contains_three_of_a_kind(dice) {
         28
     } else {
@@ -18,6 +22,14 @@ fn contains_four_of_a_kind(dice: &Dice) -> bool {
         }
     }
     false
+}
+
+fn contains_full_house(dice: &Dice) -> bool {
+    let mut counts = HashMap::new();
+    for die in dice {
+        *counts.entry(die).or_insert(0) += 1;
+    }
+    counts.values().any(|&count| count == 3) && counts.values().any(|&count| count == 2)
 }
 
 fn contains_three_of_a_kind(dice: &Dice) -> bool {
@@ -39,9 +51,16 @@ mod test {
         assert_eq!(calculate_yams_score(&dice), 28);
     }
 
+    #[test]
     fn test_four_of_a_kind() {
         let dice = [4, 4, 4, 4, 1];
         assert_eq!(calculate_yams_score(&dice), 35);
+    }
+
+    #[test]
+    fn test_full_house() {
+        let dice = [2, 2, 3, 3, 3];
+        assert_eq!(calculate_yams_score(&dice), 30);
     }
 
     #[test]
